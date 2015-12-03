3f3f19f4563ea266b4ea515412c03276ba4d42b9
Bryson Ding <bryson189@gmail.com>
2015-12-02 20:33:17 -0700

    Added functionality to add textbooks

--- textbooks.php
+++ textbooks.php
@@ -4,6 +4,8 @@
 	  	<meta charset="utf-8">
   		<meta name="viewport" content="width=device-width, initial-scale=1">
    		<link rel="stylesheet" href="css/bootstrap.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+   		<script src="js/bootstrap.js"></script>
 		<link rel="stylesheet" href="assets/stylesheets/main.css">
 		<title>Course Navigator</title> 
 	</head> 
@@ -38,7 +40,7 @@
 						<span class="glyphicon glyphicon-education"></span> Courses</button></a>
 					<a href = "professors.php"><button class="btn btn-default">
 						<span class="glyphicon glyphicon-user" ></span> Professors</button></a>
-					<a href = "textbooks.php"><button class="btn btn-default">
+					<a href = "textbook.php"><button class="btn btn-default">
 						<span class="glyphicon glyphicon-book" ></span> Textbooks</button></a>
 					<a href = "tutors.php"><button class="btn btn-default">
 						<span class="glyphicon glyphicon-blackboard" ></span> Tutors</button></a>
@@ -50,7 +52,6 @@
 				</form>
 		    </div>
 		    <div class="col-lg-10 text-left"> 
-
 			    <h1>Textbooks</h1>
 
 			    <?php 
@@ -136,9 +137,9 @@
 
 
 
-			    echo "<br><br><p>Don't see your textbook?
-			    <a href='add_textbook.php'>
-			    <button type='button' value='addtextbook' class=btn btn-default>Add a textbook</button></a></p>";
+			    echo "<br><br>
+			    <button type='button' class='btn btn-primary btn-lg' data-toggle='modal' data-target='#myModal'> 
+			    Add Textbook </button>";
 
 			    	echo '<div class="prof_list">
 			    			<a name="A_anchor"></a>
@@ -375,6 +376,61 @@
 				<a href="contactus.php">Contact Us</a>
 			</nav>
 		</footer>
+
+<div class="modal fade bs-example-modal-lg" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+  <div class="modal-dialog " role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="myModalLabel"> <strong> Add a textbook </strong></h4>
+      </div>
+      <div class="modal-body">
+ 				<form action = "upload-textbook-profile.php" method = "post" class="form-horizontal" enctype="multipart/form-data">
+				  <div class="col-xs-6 col-md-3">
+				    <a class="thumbnail">
+				      <img src="assets/images/default-book.png" alt="book picture" width="200" height="200" >
+				    </a>
+				  </div>
+				    <br> Upload a textbook picture: <br><br>
+				    <input type="file" name="fileToUpload" id="fileToUpload">
+				    <br><br><br><br>
+				  <div class="form-group">
+				    <label for="inputname" class="col-sm-2 control-label">Book Name</label>
+				    <div class="col-sm-10">
+					  <textarea class="form-control" name = "name" rows="2" required></textarea>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="inputname" class="col-sm-2 control-label">Courses </label>
+				    <div class="col-sm-10">
+					  <textarea class="form-control" name = "courses" rows="3"></textarea></textarea>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="inputname" class="col-sm-2 control-label">ISBN</label>
+				    <div class="col-sm-10">
+				      <input type="number" name = "isbn" class="form-control" required>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <label for="inputname" class="col-sm-2 control-label">Edition Number</label>
+				    <div class="col-sm-10">
+				      <input type="number" name = "editionnum" class="form-control" required>
+				    </div>
+				  </div>
+				  <div class="form-group">
+				    <div class="col-sm-offset-2 col-sm-10">
+				      <button type="submit" value = "Submit" class="btn btn-primary"><strong>Submit </strong></button>
+				    </div>
+				  </div>
+				</form>  
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 	</body> 
 
