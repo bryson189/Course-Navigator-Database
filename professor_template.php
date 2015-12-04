b59aa9be097f3f721c4df434c2f04fe156ce49c2
Tai Lai <ctlai69@gmail.com>
2015-12-03 22:12:04 -0700

    fixed more mysql_connect errors

--- professor_template.php
+++ professor_template.php
@@ -71,7 +71,7 @@ if(isset($_SESSION['email'])){
 		    <div class="col-lg-10 text-left"> 
 
 		    	<?php 
-					$db = mysql_connect("localhost","root","default");
+					$db = @mysql_connect("localhost","root","default");
 					if(!$db){
 						die("MySQL connection error. " . mysql_error());
 					}
